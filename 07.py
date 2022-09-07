2dbc16583501ea0f1e8a2b1795c9e9ad9f3ad2e7
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-09-07 16:24:05 -0300

    1st v

---
+++ 07.py
@@ -0,0 +1,9 @@
+#7
+#programador: Eduardo Costa
+#data da última atualização: 04/09/2022
+
+ano = int(input('Forneça um ano: '))
+if (ano%400==0) or ((ano % 4 == 0) and (a % 100!=0)):
+  print('Ano bissexto.')
+else:
+  print('Ano não é bissexto.') 
