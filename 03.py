0317cf18059511d58d5103ba2af6f25407c6d438
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-09-07 16:20:42 -0300

    1st v

---
+++ 03.py
@@ -0,0 +1,21 @@
+#3
+#programador: Eduardo Costa
+#data da última atualização: 04/09/2022
+
+dia = int (input('Número correspondente ao dia da semana: '))
+if dia == 1:
+  print('Domingo')
+elif dia == 2:
+  print('Segunda-feira')
+elif dia == 3:
+  print('Terça-feira')
+elif dia == 4:
+  print('Quarta-feira')
+elif dia == 5:
+  print('Quinta-feira')
+elif dia == 6:
+  print('Sexta-feira')
+elif dia == 7:
+  print('Sábado')
+else:
+  print('Erro: insira um número válido (1-7)')
