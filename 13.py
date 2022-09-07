c5f2d3fc7b15a57b39b2db0e394651d99a731fc6
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-09-07 17:06:33 -0300

    1st v

---
+++ 13.py
@@ -0,0 +1,10 @@
+#13
+#programador: Eduardo Costa
+#data da última atualização: 04/09/2022
+
+n = float(input('Forneça um número.'))
+
+if n % 1 == 0:
+  print('Inteiro.')
+else:
+  print('Decimal')
