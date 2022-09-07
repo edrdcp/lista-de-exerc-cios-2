d4baaae27e42af27419c4fbb96ff550fb6861f59
edrdcp <112147320+edrdcp@users.noreply.github.com>
2022-09-07 17:06:06 -0300

    1st v

---
+++ 12.py
@@ -0,0 +1,10 @@
+#12
+#programador: Eduardo Costa
+#data da última atualização: 04/09/2022
+
+n = int(input('Forneça um número inteiro.'))
+
+if n % 2 == 0:
+  print('Par.')
+else:
+  print('Ímpar.')
